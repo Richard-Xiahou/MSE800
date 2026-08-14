27be27c6d896500cc990fd6bd4da099fb2f2c5e0
xiahou5 <3121588532@qq.com>
2026-08-15 09:22:48 +1200

    Week 3- Activity 1: File data processing v1.0

---
+++ week03/pyProject/Iris.py
@@ -0,0 +1,33 @@
+
+
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+iris = fetch_ucirepo(id=53) 
+# https://archive.ics.uci.edu/dataset/53/iris
+  
+# data (as pandas dataframes) 
+X = iris.data.features 
+y = iris.data.targets 
+  
+# metadata 
+# print(iris.metadata) 
+  
+# variable information 
+# print(iris.variables) 
+
+# Total number of records
+print("Total records:", len(X))
+
+# Different flower available
+print("Different flowers:")
+# for flower in y:
+#    print(flower["class"])
+flowers = set(y["class"])
+print(len(flowers))
+
+# Names of all flowers
+print("Flower names:")
+for flower in flowers:
+    print(flower)
+
