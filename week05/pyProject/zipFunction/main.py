01bc45166f9fbe0b02a24b7ce9bb6e428611d3c8
xiahou5 <3121588532@qq.com>
2026-08-30 08:53:47 +1200

    W5A2 Merging Dictionaries with the Double Asterisk (**) v1.0

--- week05/pyProject/zipFunction/main.py
+++ week05/pyProject/zipFunction/main.py
@@ -1,12 +1,35 @@
-keys = ["a","b","c"]
-values = [1,2,3]
-dictionary = {k:v for k,v in zip(keys,values)}
 
-print(dictionary)
-# {'a': 1, 'b': 2, 'c': 3}
+# Week 5 - Activity 1: Zip function - Dictionary data type - Tips and tricks
+def tips_and_tricks():
+    keys = ["a","b","c"]
+    values = [1,2,3]
+    dictionary = {k:v for k,v in zip(keys,values)}
 
-keys.append("d")
-values.append(4);
-dictionary = {k:v for k,v in zip(keys,values)}
-print(dictionary)
-# {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+    print(dictionary)
+    # {'a': 1, 'b': 2, 'c': 3}
+
+    keys.append("d")
+    values.append(4);
+    dictionary = {k:v for k,v in zip(keys,values)}
+    print(dictionary)
+    # {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+
+# Week 5 - Activity 2: Merging Dictionaries with Conditions
+def mergeDics():
+    Key1 = ["a", "b", "c", "d", "f", "g", "h", "e", "a"]
+    Value1= [20, 3, 1, 88, 55, 92, 6, 90, 910]
+    Key2=["u", "b", 'o', 'x',  "e", 'a']
+    Value2=[200, 30, 10, 88, 55, 920]
+    dic1 = {k:v for k,v in zip(Key1,Value1)}
+    dic2 = {k:v for k,v in zip(Key2,Value2)}
+
+    merged_dict = {**dic1, **dic2}
+    print(merged_dict);
+
+if __name__ == "__main__":
+    # tips_and_tricks()
+    mergeDics();
+
+
+
